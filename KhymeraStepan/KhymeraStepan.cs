f4e9fc6b8dbe096fa3a62c6096b3ba507c54fc26
StepanKhymera <44585663+StepanKhymera@users.noreply.github.com>
2020-10-10 20:58:28 +0300

    added basic tutorial example

--- KhymeraStepan/KhymeraStepan.cs
+++ KhymeraStepan/KhymeraStepan.cs
@@ -19,21 +19,72 @@ namespace KhymeraStepan
         {
             get { return "StepanKhymera"; }
         }
-        RobotCommand IRobotAlgorithm.DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
-        {
-            var newpos = robots[robotToMoveIndex].Position;
-            ++newpos.X;
-            ++newpos.Y;
-
-            return new MoveCommand() { NewPosition = newpos};
-        }
-
         private void Logger_OnLogRound(object sender, LogRoundEventArgs e)
         {
             ++round;
         }
 
+        RobotCommand IRobotAlgorithm.DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            
+             Robot.Common.Robot movingRobot = robots[robotToMoveIndex];
+             if ((movingRobot.Energy > 500) && (robots.Count < map.Stations.Count))
+             {
+                 return new CreateNewRobotCommand();
+             }
 
+             Position stationPosition = FindNearestFreeStation(robots[robotToMoveIndex], map, robots);
+
+                 if (stationPosition == null)
+                 return null;
+             if (stationPosition == movingRobot.Position)
+                 return new CollectEnergyCommand();
+             else
+             {
+                 return new MoveCommand() { NewPosition = stationPosition };
+             }    
+
+        }
+
+        public bool IsCellFree(Position cell, Robot.Common.Robot movingrobot,  IList<Robot.Common.Robot> robots)
+        {
+            foreach(var robot in robots)
+            {
+                if (robot.Position == cell)
+                {
+                    if(robot != movingrobot)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public static int FindDistance(Position a, Position b)
+        {
+            return (int)(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+        }
+        public Position FindNearestFreeStation(Robot.Common.Robot movingRobot, Map map, IList<Robot.Common.Robot> robots)
+        {
+            EnergyStation nearest = null;
+            int minDistance = int.MaxValue;
+            foreach (var station in map.Stations)
+            {
+                if (IsCellFree(station.Position, movingRobot, robots))
+                {
+                    int d = FindDistance(station.Position, movingRobot.Position);
+                    if (d < minDistance)
+
+                    {
+
+                        minDistance = d;
+                        nearest = station;
+
+                    }
+                }
+            }
+            return nearest == null ? null : nearest.Position;
+        }
 
 
     }
